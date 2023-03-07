03a6c3226f7ecf0a534d7a9cbfd6d8027b78fcf0
StefanCodeClan <stefan.de.waard@codeclan.nl>
2023-03-07 19:12:23 +0100

    latest tests

--- src/app/features/form/components/container/container.component.spec.ts
+++ src/app/features/form/components/container/container.component.spec.ts
@@ -1,25 +1,45 @@
 import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {Router} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
-import {provideMockStore} from '@ngrx/store/testing';
-import {BOOKS_TEST_STATE} from '../../../../utils/testing/books.test.util';
+import {Store} from '@ngrx/store';
+import {CreateBook} from '../../../../core/stores/books/books.actions';
+import {IBook} from '../../../../utils/types/book.types';
 
 import {ContainerComponent} from './container.component';
 
 describe('ContainerComponent', () => {
   let component: ContainerComponent;
   let fixture: ComponentFixture<ContainerComponent>;
-  const initialState = {...BOOKS_TEST_STATE};
-
+  let router: Router;
+  const store = {
+    dispatch: jest.fn(),
+  };
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ContainerComponent],
-      imports: [RouterTestingModule, ReactiveFormsModule, FormsModule],
-      providers: [provideMockStore({initialState})],
+      imports: [
+        RouterTestingModule,
+        ReactiveFormsModule,
+        FormsModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+      ],
+      providers: [
+        {
+          provide: Store,
+          useValue: store,
+        },
+        MatDatepickerModule,
+        MatNativeDateModule,
+        FormBuilder,
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
     }).compileComponents();
-
+    router = TestBed.inject(Router);
     fixture = TestBed.createComponent(ContainerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -27,5 +47,32 @@ describe('ContainerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('form', () => {
+    beforeEach(() => {
+      component.ngOnInit();
+      fixture.detectChanges();
+    });
+    it('should create a form', () => {
+      expect(component.form).toBeDefined();
+    });
+    it('invalid when empty', () => {
+      expect(component.form.valid).toBeFalsy();
+    });
+  });
+  describe('submit', () => {
+    let navigateSpy;
+    beforeEach(() => {
+      navigateSpy = jest.spyOn(router, 'navigate');
+      component.ngOnInit();
+      component.submit();
+      fixture.detectChanges();
+    });
+    it('should dispatch', () => {
+      expect(store.dispatch).toBeCalledWith(CreateBook({data: component.form.value as IBook}));
+    });
+    it('should navigate', () => {
+      expect(navigateSpy).toBeCalledWith(['/']);
+    });
   });
 });
